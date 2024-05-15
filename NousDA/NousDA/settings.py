5ba82ecb1cd324676086e4ab0397b5def6538fff
Jai Suphavadeeprasit <jaisehgal11299@gmail.com>
2024-05-14 23:42:05 -0400

    Typo

--- NousDA/NousDA/settings.py
+++ NousDA/NousDA/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'django-db'
-CELERY_ACCEPT_CONTET = ['json']
+CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'America/New_York'
